7f862f6b1c9b911cf95bfc29c7203d78b61727dd
Rafael Pintar Alevato <rpa.alevato@gmail.com>
2018-03-05 08:36:44 -0300

    Apt update was missing the public key for sublime. These changes were not tested in a new linux install and still needs tests.

--- Linux/Ubuntu/sublime-text-3.sh
+++ Linux/Ubuntu/sublime-text-3.sh
@@ -7,6 +7,7 @@ printf "**************************************************\n\n"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-get -y install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F57D4F59BD3DF454
 sudo apt-get update
 sudo apt-get -y install sublime-text
 mkdir ~/.config/sublime-text-3/Packages/User
