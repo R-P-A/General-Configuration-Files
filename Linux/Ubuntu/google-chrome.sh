31a06d999dc066d7cd3abc759fa8ce867335805e
Rafael Pintar Alevato <rpa.alevato@gmail.com>
2018-03-05 08:29:39 -0300

    Changed google-chrome link. Google disabled the old link and this is the one that works now.

--- Linux/Ubuntu/google-chrome.sh
+++ Linux/Ubuntu/google-chrome.sh
@@ -4,6 +4,7 @@ printf "\n\n\n**************************************************\n"
 printf "*           Installing Google Chrome             *\n"
 printf "**************************************************\n\n"
 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome*.deb
-rm -rf google-chrome*.deb
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo apt update
+sudo apt-get install google-chrome-stable
