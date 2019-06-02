95569e668f154547d3adce382f3ca63df39f6f81
Rafael Pintar Alevato <rpa.alevato@gmail.com>
2019-06-02 18:33:14 -0300

    Remove apparent useless line

--- Linux/Ubuntu/vscode.sh
+++ Linux/Ubuntu/vscode.sh
@@ -6,6 +6,5 @@ printf "**************************************************\n\n"
 
 sudo apt update
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update
 sudo apt -y install code
